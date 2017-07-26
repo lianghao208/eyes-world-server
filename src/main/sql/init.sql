138221e8539a2e3c3158a2a023eb4c5858d4818d
JeremyTang999 <iamtmh@163.com>
2017-07-26 10:00:59 +0800

    creation and modification time  added in database field

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -13,33 +13,43 @@ USE tvos;
 CREATE TABLE user(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	username VARCHAR(255),
-	password VARCHAR(255)
+	password VARCHAR(255),
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --用户信息表
 CREATE TABLE user_info(
 	user_id INT PRIMARY KEY,
-	gender VARCHAR(16)
+	gender VARCHAR(16),
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --省份表
 CREATE TABLE province(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	province_name VARCHAR(32),
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --城市表
 CREATE TABLE city(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	city_name VARCHAR(255),
-	province_id INT
+	province_id INT,
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --高校表
 CREATE TABLE college(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	college_name VARCHAR(255),
-	province_id INT
+	province_id INT,
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --相册表
@@ -50,7 +60,8 @@ CREATE TABLE album(
 	college_id INT,
 	visit_amount INT,
 	like_amount INT,
-	creation_time BIGINT
+	creation_time BIGINT,
+	modification_time BIGINT
 );
 
 --图片表
@@ -60,14 +71,16 @@ CREATE TABLE photo(
 	album_id INT,
 	user_id INT,
 	like_amount INT,
-	creation_time BIGINT
+	creation_time BIGINT,
+	modification_time BIGINT
 )
 
 CREATE TABLE photo_like(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	photo_id INT,
 	user_id INT,
-	creation_time BIGINT
+	creation_time BIGINT,
+	modification_time BIGINT
 ); 
 
 CREATE TABLE photo_comment(
@@ -75,5 +88,6 @@ CREATE TABLE photo_comment(
 	photo_id INT,
 	user_id INT,
 	content TEXT,
-	creation_time BIGINT
+	creation_time BIGINT,
+	modification_time BIGINT
 );
